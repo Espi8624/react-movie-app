110964e132dea3faf1f354b0528ed1678b270521
sp y <espi8624@gmail.com>
2022-09-26 00:11:41 +0900

    Add styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
 
         <Route path="/movie/:id" element={<Detail />} />
